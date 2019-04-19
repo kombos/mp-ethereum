6fb50907d72669c75efc139a27671cc40cee5428
kombos <18464450+kombos@users.noreply.github.com>
2019-04-19 20:57:05 +0530

    Updates

---
+++ precache-manifest.03368d0208332a909e6aa1912287a4a7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac0dfbe3e0b34a7d479",
+    "url": "/static/css/main.bb39a331.chunk.css"
+  },
+  {
+    "revision": "2ac0dfbe3e0b34a7d479",
+    "url": "/static/js/main.ce8abf5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f092e821ebbd8669c8df",
+    "url": "/static/js/2.bb6fb93f.chunk.js"
+  },
+  {
+    "revision": "0d1e8157693326cdc8fbf0905080528c",
+    "url": "/static/media/MultiprizerLogo.0d1e8157.png"
+  },
+  {
+    "revision": "9902dee517428a9c3b93b94ad060bcf5",
+    "url": "/static/media/MultiprizerLogoBlue.9902dee5.png"
+  },
+  {
+    "revision": "8456e7c2d13f26f6f24260c4d4beece8",
+    "url": "/static/media/headerStrip.8456e7c2.png"
+  },
+  {
+    "revision": "25567053f7e23e40fa69c5e0469f513b",
+    "url": "/static/media/MultiprizerLogo.25567053.png"
+  },
+  {
+    "revision": "f72dc231a7bf4c87106af2d1b766b52a",
+    "url": "/static/media/bg1.f72dc231.png"
+  },
+  {
+    "revision": "32455ac80aae06f34963163bafb5be5a",
+    "url": "/static/media/foooterStrip-01.32455ac8.png"
+  },
+  {
+    "revision": "fa2cbd9b6eebbacbaf2c07501a3aede1",
+    "url": "/static/media/headerStrip44.fa2cbd9b.png"
+  },
+  {
+    "revision": "d0b15284489e45c2564f11e9c83cf305",
+    "url": "/static/media/headerStripr.d0b15284.png"
+  },
+  {
+    "revision": "f8db970a937d97d7440a21e22d6cbdaa",
+    "url": "/static/media/w8.f8db970a.png"
+  },
+  {
+    "revision": "ef2f5a60d7f445227cb87dcc05641226",
+    "url": "/static/media/bg.ef2f5a60.jpg"
+  },
+  {
+    "revision": "08fa2f96e52531208de5023d7d6a49ce",
+    "url": "/static/media/bg102.08fa2f96.jpg"
+  },
+  {
+    "revision": "e156fa3b6f6102bdd2d94edaa1c63a4b",
+    "url": "/static/media/bg103.e156fa3b.jpg"
+  },
+  {
+    "revision": "a0b1d5a10ae6f04e8cf1b70180d143de",
+    "url": "/static/media/bg104.a0b1d5a1.jpg"
+  },
+  {
+    "revision": "83ac2374a50915391eaef281bb1257b8",
+    "url": "/static/media/bg105.83ac2374.jpg"
+  },
+  {
+    "revision": "8b0ed0c1e6d16f501361f63bebc21b77",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4be23d9444cd54bb0f3e8adbc482bd64.js"
+  "/precache-manifest.03368d0208332a909e6aa1912287a4a7.js"
 );
 
 workbox.clientsClaim();
